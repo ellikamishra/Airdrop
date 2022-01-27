543f1ed5fb2b1b8b181355303ffec05d58d10825
ellikamishra <ellikamishra009@gmail.com>
2022-01-27 17:37:51 +0530

    Nft bundle deployed

--- DAOStarter/src/App.jsx
+++ DAOStarter/src/App.jsx
@@ -1,9 +1,35 @@
+
+
+import { useEffect,useMemo,useSate } from "react";
+
+//import thirdweb
+import {useWeb3} from "@3rdweb/hooks";
+
+
+
 const App = () => {
+
+
+  const{connectWallet,address,error,provider}=useWeb3();
+  console.log("Address:",address)
+
+  //Address of wallet unknown
+  if(!address){
+    return(<div className="landing">
+    <h1>Welcome to DAO Chai Ki Tapri</h1>
+    <button onClick={()=>connectWallet("injected")} className="btn-hero">
+
+      Connect your wallet
+    </button>
+  </div>);
+    
+  }
+
+  //Address of user known
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
-    </div>
-  );
+      <h1>👀 wallet connected, now what!</h1>
+    </div>);
 };
 
 export default App;
